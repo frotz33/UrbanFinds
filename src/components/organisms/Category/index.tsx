afc15946d402b2ffe087e737a3903e2c8946b17c
frotz33 <krystian.stelmach3@gmail.com>
2023-09-13 20:36:15 +0200

    feat: Add h2 on category page with category names and style them

--- src/components/organisms/Category/index.tsx
+++ src/components/organisms/Category/index.tsx
@@ -3,16 +3,19 @@ import { ProductTile } from '@/components/molecules/ProductTile';
 import React from 'react';
 import styles from './index.module.scss';
 import { ICategoryProps } from './types';
-const { products__wrapper } = styles;
+const { products__wrapper, products__category } = styles;
 
 export const Category = ({ category, products }: ICategoryProps) => {
   return (
-    <div className={products__wrapper}>
-      {products.map((product) => {
-        if (product.category === category) {
-          return <ProductTile key={product.id} productInfo={product} />;
-        }
-      })}
-    </div>
+    <>
+      <div className={products__wrapper}>
+        <h2 className={products__category}>{category}</h2>
+        {products.map((product) => {
+          if (product.category === category) {
+            return <ProductTile key={product.id} productInfo={product} />;
+          }
+        })}
+      </div>
+    </>
   );
 };
